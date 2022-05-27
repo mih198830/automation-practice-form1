483985071c6b86250c3a1930e7b410acb5cfa246
mikhail.matskevich <mih198830@gmail.com>
2022-05-28 00:00:02 +0400

    methods added 2

--- src/test/java/tests/AutomationPracticeForm1.java
+++ src/test/java/tests/AutomationPracticeForm1.java
@@ -24,7 +24,6 @@ public class AutomationPracticeForm1 {
         $("label[for=gender-radio-1]").click();
         $("#userNumber").sendKeys("123456789");
         $("dateOfBirthInput").click();
-        $(".subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3").sendKeys("Geography, Law");
-
+        $(By.className("subjects-auto-complete__control")).sendKeys("Law");
     }
 }
