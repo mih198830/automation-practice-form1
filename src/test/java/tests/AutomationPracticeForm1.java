67091346daaf8bd27601eac9a3abc7eba3b87389
mikhail.matskevich <mih198830@gmail.com>
2022-05-27 23:39:06 +0400

    methods added

--- src/test/java/tests/AutomationPracticeForm1.java
+++ src/test/java/tests/AutomationPracticeForm1.java
@@ -3,7 +3,9 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
@@ -16,5 +18,13 @@ public class AutomationPracticeForm1 {
     @Test
     void PageTest() {
         open("/automation-practice-form");
+        $("#firstName").sendKeys("Mikhail");
+        $("#lastName").sendKeys("Matskevich");
+        $("#userEmail").sendKeys("myemail@gmail.com");
+        $("label[for=gender-radio-1]").click();
+        $("#userNumber").sendKeys("123456789");
+        $("dateOfBirthInput").click();
+        $(".subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3").sendKeys("Geography, Law");
+
     }
 }
