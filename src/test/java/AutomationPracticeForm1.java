c018acfa4f86e4542c99502274612a8e71e162e5
mikhail.matskevich <mih198830@gmail.com>
2022-05-28 22:40:07 +0400

    methods added 4

--- src/test/java/AutomationPracticeForm1.java
+++ src/test/java/AutomationPracticeForm1.java
@@ -26,23 +26,60 @@ public class AutomationPracticeForm1 {
         executeJavaScript("$('#RightSide_Advertisement').remove()");
         executeJavaScript("$('.i-amphtml-fill-content').remove()");
 
+        //first name last name
         $("#firstName").setValue("Mikhail");
         $("#lastName").setValue("Matskevich");
-        $("#userEmail").setValue("myemail@gmail.com");
-        $("[for=gender-radio-1]").click();
-        $("#userNumber").setValue("123456789");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__day--029").click();
-        $(".col-md-9").click();
-        $("#subjectsInput").setValue("Physics").pressEnter();
-        $("[for=hobbies-checkbox-2]").click();
-        $("#uploadPicture").uploadFile(new File("src/test/java/tests/selenidescreen.png"));
 
+        //email
+        $("#userEmail").setValue("myemail@gmail.com");
+
+        //radio-button
+        $("#genterWrapper").$(byText("Male")).click();
+
+        //phone number
+        $("#userNumber").setValue("123456789");
+
+        //dob
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("June");
+        $(".react-datepicker__year-select").selectOption("1988");
+        $(".react-datepicker__day--029").click();
+
+        //subject
+        $("#subjectsInput").sendKeys("E");
+        $(byText("English")).click();
+
+        //checkbox
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+
+        //upload
+        $("#uploadPicture").uploadFile(new File("src/test/java/resources/selenidescreen.png"));
+
+        //address
         $("#currentAddress").setValue("Georgia, stret1");
+
         $("#state").scrollTo().click();
+
+        //state
         $("#stateCity-wrapper").$(Selectors.byText("NCR")).click();
+
+        //city
         $("#city").click();
         $("#stateCity-wrapper").$(Selectors.byText("Delhi")).click();
 
+        $("#submit").click();
+
+//        $(".practice-form-wrapper").shouldHave(
+//                text("Mikhail"),
+//                text("Matskevich"),
+//                text("myemail@gmail.com"),
+//                text("Male"),
+//                text("123456789"),
+//                text(""),
+//
+//        )
     }
 }
