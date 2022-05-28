15165dc02323e3b9902b6a6386ad203c0464eee1
mikhail.matskevich <mih198830@gmail.com>
2022-05-28 14:32:23 +0400

    methods added 2

--- src/test/java/tests/AutomationPracticeForm1.java
+++ src/test/java/tests/AutomationPracticeForm1.java
@@ -5,25 +5,35 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 
 
 public class AutomationPracticeForm1 {
     @BeforeAll
     static void BeforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
     }
 
     @Test
     void PageTest() {
         open("/automation-practice-form");
-        $("#firstName").sendKeys("Mikhail");
-        $("#lastName").sendKeys("Matskevich");
-        $("#userEmail").sendKeys("myemail@gmail.com");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('#RightSide_Advertisement').remove()");
+
+
+        $("#firstName").setValue("Mikhail");
+        $("#lastName").setValue("Matskevich");
+        $("#userEmail").setValue("myemail@gmail.com");
         $("label[for=gender-radio-1]").click();
-        $("#userNumber").sendKeys("123456789");
-        $("dateOfBirthInput").click();
-        $(By.className("subjects-auto-complete__control")).sendKeys("Law");
+        $("#userNumber").setValue("123456789");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__day--029").click();
+        executeJavaScript("$('arguments[0].scrollIntoView(true)').");
+//      $(".css-1wa3eu0-placeholder").parent().setValue("skdf");
+        $(".custom-control-label").click();
+        $("#currentAddress").setValue("Georgia, stret1");
     }
 }
